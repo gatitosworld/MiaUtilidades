252ed14371dfa908a6e352f2463ba072dc7c7367
Mathéo <soyzer0@protonmail.com>
2022-06-30 02:27:17 +0200

    Ajustes con las IDs de los usuarios GOD.

--- src/eventos/messageCreate.js
+++ src/eventos/messageCreate.js
@@ -15,13 +15,15 @@ module.exports = {
         let arr = await db.get('wl'); // Array  
         let whl = gw.whitelist(message.content, arr); 
 
-        let matt = "21674692307006259"; let cap = "408728483808673792";
+        let matt = "216746923070062593"; 
+        let cap = "408728483808673792";
         let ticket_parent = "827214252787499050";
         let staff_parent = "849284765554442270";
 
         if(link && message.channel.parentId == ticket_parent) return console.log("[AntiLinks] Se ha enviado en un ticket. Autorizado.");
         if(link && message.channel.parentId == staff_parent) return console.log("[AntiLinks] Se ha enviado en un canal de staff. Autorizado.")
-        if(link && message.author.id === cap) return console.log("[AntiLinks] El/los usuario/s está/n autorizado/s.");  if(link && message.author.id === matt) return console.log("[AntiLinks] El/los usuario/s está/n autorizado/s.");
+        if(link && message.author.id === cap) return console.log("[AntiLinks] El/los usuario/s está/n autorizado/s.");  
+        if(link && message.author.id === matt) return console.log("[AntiLinks] El/los usuario/s está/n autorizado/s.");
         if(whl) return;
 
         if(link) {
