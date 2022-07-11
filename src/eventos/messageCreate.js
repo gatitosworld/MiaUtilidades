0fffa752c37577a5afd1f11d521027e12fc6c146
Mathéo <96652538+soyzer0@users.noreply.github.com>
2022-07-12 00:04:00 +0200

    Prevención de errores y correción de error fatal
    BUG: al borrar un mensaje que MiaUtilidades ha detectado como no permitido antes de que el cleinte lo elimine este crashea.

--- src/eventos/messageCreate.js
+++ src/eventos/messageCreate.js
@@ -6,19 +6,18 @@ module.exports = {
     name: 'messageCreate',
     async execute(client, db, message) {
 
-if(message.author.bot) return;
-if(message.member.permissions.has("ADMINISTRATOR")) return;
+        if (message.author.bot) return;
+        if (message.member.permissions.has("ADMINISTRATOR")) return;
 
 
-        let link = gw.links(message.content);  
+        let link = gw.links(message.content);
 
-let mods_twitch = "868256143938564136"
-let ticket_parent = "827214252787499050";
-let staff_parent = "849284765554442270";
-if(message.channel.id == mods_twitch) return console.log("[AntiLinks] El mensaje se ha enviado en un canal autorizado.")
-if (message.channel.parentId == ticket_parent) return console.log("[AntiLinks] Se ha enviado en un ticket. Autorizado.");
-if (message.channel.parentId == staff_parent) return console.log("[AntiLinks] Se ha enviado en un canal de staff. Autorizado.");
-if(walltext && message.channel.id == "851869089639301211" || wltxflood && message.channel.id == "851869089639301211") return console.log("[AntiLinks] El mensaje se ha enviado en un canal autorizado."); // Roleplay
+        let mods_twitch = "868256143938564136"
+        let ticket_parent = "827214252787499050";
+        let staff_parent = "849284765554442270";
+        if (message.channel.id == mods_twitch) return console.log("[AntiLinks] El mensaje se ha enviado en un canal autorizado.")
+        if (message.channel.parentId == ticket_parent) return console.log("[AntiLinks] Se ha enviado en un ticket. Autorizado.");
+        if (message.channel.parentId == staff_parent) return console.log("[AntiLinks] Se ha enviado en un canal de staff. Autorizado.");
 
         const embed_links = new MessageEmbed()
             .setTitle("¡No se permiten links externos en GatitosWorld!")
@@ -27,13 +26,13 @@ if(walltext && message.channel.id == "851869089639301211" || wltxflood && messag
         const embed_waltext = new MessageEmbed()
             .setTitle("¡Trata de no mandar mensajes con tantas líneas que puedan saturar la pantalla!")
             .setColor(ajustes.colores.incorrecto)
- 
+
         let arr = await db.get('wl'); // Array  
         let whl = gw.whitelist(message.content, arr);
         let walltext = gw.walltext(message.content);
         let wltxflood = gw.wltxflood(message.content);
+        if (walltext && message.channel.id == "851869089639301211" || wltxflood && message.channel.id == "851869089639301211") return console.log("[AntiLinks] El mensaje se ha enviado en un canal autorizado."); // Roleplay
 
-        
         if (whl) return;
 
         if (link) {
@@ -41,27 +40,39 @@ if(walltext && message.channel.id == "851869089639301211" || wltxflood && messag
                 .then(msg => {
                     setTimeout(() => msg.delete(), 3000)
                 });
-            setTimeout(() => message.delete(), 3000)
-        }; 
+            try {
+                setTimeout(() => message.delete(), 3000);
+            } catch (e) {
+                console.error(e);
+            }
+        };
 
 
-        if(walltext) {
+        if (walltext) {
             message.reply({ embeds: [embed_waltext] })
-            .then(msg => {
-                setTimeout(() => msg.delete(), 3000);
-            });
+                .then(msg => {
+                    setTimeout(() => msg.delete(), 3000);
+                });
 
-            setTimeout(() => message.delete(), 3000);  
+            try {
+                setTimeout(() => message.delete(), 3000);
+            } catch (e) {
+                console.error(e);
+            }
         }
 
-        if(wltxflood && message.content.length > 200) {
-            if(message.content == "") return;
+        if (wltxflood && message.content.length > 200) {
+            if (message.content == "") return;
             message.reply({ embeds: [embed_waltext] })
-            .then(msg => {
-                setTimeout(() => msg.delete(), 3000);
-            });
+                .then(msg => {
+                    setTimeout(() => msg.delete(), 3000);
+                });
 
-            setTimeout(() => message.delete(), 3000);
+            try {
+                setTimeout(() => message.delete(), 3000);
+            } catch (e) {
+                console.error(e);
+            }
         }
 
     }
