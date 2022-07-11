ac3e63fc979a4685ff59737ce024b9c49daffe86
Mathéo <96652538+soyzer0@users.noreply.github.com>
2022-07-11 18:18:43 +0200

    Módulo completo antiwalltext
    - Aplicación de GatitosWorld API

--- src/eventos/messageCreate.js
+++ src/eventos/messageCreate.js
@@ -6,35 +6,62 @@ module.exports = {
     name: 'messageCreate',
     async execute(client, db, message) {
 
+if(message.author.bot) return;
+//if(message.member.permissions.has("ADMINISTRATOR")) return;
+
+
+        let link = gw.links(message.content);  
+
+let mods_twitch = "868256143938564136"
+let ticket_parent = "827214252787499050";
+let staff_parent = "849284765554442270";
+if(link && message.channel.id == mods_twitch) return console.log("[AntiLinks] El mensaje se ha enviado en un canal autorizado.")
+if (link && message.channel.parentId == ticket_parent) return console.log("[AntiLinks] Se ha enviado en un ticket. Autorizado.");
+if (link && message.channel.parentId == staff_parent) return console.log("[AntiLinks] Se ha enviado en un canal de staff. Autorizado.")
+
         const embed_links = new MessageEmbed()
             .setTitle("¡No se permiten links externos en GatitosWorld!")
             .setColor(ajustes.colores.incorrecto)
 
-        let link = gw.links(message.content);   
+        const embed_waltext = new MessageEmbed()
+            .setTitle("¡Trata de no mandar mensajes con tantas líneas que puedan saturar la pantalla!")
+            .setColor(ajustes.colores.incorrecto)
+ 
         let arr = await db.get('wl'); // Array  
         let whl = gw.whitelist(message.content, arr);
+        let walltext = gw.walltext(message.content);
+        let wltxflood = gw.wltxflood(message.content);
 
-        let matt = "216746923070062593";
-        let cap = "408728483808673792";
-        let ticket_parent = "827214252787499050";
-        let staff_parent = "849284765554442270";
-
-        if (link && message.channel.parentId == ticket_parent) return console.log("[AntiLinks] Se ha enviado en un ticket. Autorizado.");
-        if (link && message.channel.parentId == staff_parent) return console.log("[AntiLinks] Se ha enviado en un canal de staff. Autorizado.")
-        if (link && message.author.id === cap) return console.log("[AntiLinks] El/los usuario/s está/n autorizado/s.");
-        if (link && message.author.id === matt) return console.log("[AntiLinks] El/los usuario/s está/n autorizado/s.");
+        
         if (whl) return;
 
         if (link) {
-
             message.reply({ embeds: [embed_links] })
                 .then(msg => {
                     setTimeout(() => msg.delete(), 3000)
                 });
-
             setTimeout(() => message.delete(), 3000)
+        }; 
 
-        };
+
+        if(walltext) {
+            message.reply({ embeds: [embed_waltext] })
+            .then(msg => {
+                setTimeout(() => msg.delete(), 3000);
+            });
+
+            setTimeout(() => message.delete(), 3000);  
+        }
+
+        if(wltxflood && message.content.length > 200) {
+            if(message.content == "") return;
+            message.reply({ embeds: [embed_waltext] })
+            .then(msg => {
+                setTimeout(() => msg.delete(), 3000);
+            });
+
+            setTimeout(() => message.delete(), 3000);
+        }
 
     }
 
